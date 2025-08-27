74c77eea6a2c60b4cedb2ef2d68d8e14818ad900
nolan227 <127327905+nolan227@users.noreply.github.com>
2025-08-27 19:18:49 +0200

    Add files via upload

---
+++ Monbot/index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.once("ready", () => {
+  console.log(`✅ Connecté en tant que ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content === "!ping") {
+    message.reply("Pong 🏓");
+  }
+});
+
+client.login(process.env.TOKEN); // Utilise la variable d'environnement TOKEN
